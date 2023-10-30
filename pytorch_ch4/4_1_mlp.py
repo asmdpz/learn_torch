e95c1b286975d66699ba8a7c9e12bf04108ed7f7
asmdpz <1633099191@qq.com>
2023-10-30 19:17:10 +0800

    提交代码

---
+++ pytorch_ch4/4_1_mlp.py
@@ -0,0 +1,37 @@
+import torch
+from d2l import torch as d2l
+
+# 4.1 多层感知机
+# 4.1.1 隐藏层
+# 4.1.2 激活函数
+# relu函数
+x = torch.arange(-8.0, 8.0, 0.1, requires_grad=True)
+y = torch.relu(x)
+d2l.plot(x.detach(), y.detach(), 'x', 'relu(x)', figsize=(5, 2.5))
+d2l.plt.show()
+# y不是标量，所以需要指定gradient参数
+y.backward(torch.ones_like(x), retain_graph=True)
+d2l.plot(x.detach(), x.grad, 'x', 'grad of relu', figsize=(5, 2.5))
+d2l.plt.show()
+
+# sigmoid函数
+y = torch.sigmoid(x)
+d2l.plot(x.detach(), y.detach(), 'x', 'sigmoid(x)', figsize=(5, 2.5))
+d2l.plt.show()
+# 清除以前的梯度
+x.grad.data.zero_()
+y.backward(torch.ones_like(x), retain_graph=True)
+d2l.plot(x.detach(), x.grad, 'x', 'grad of sigmoid', figsize=(5, 2.5))
+d2l.plt.show()
+
+# tanh函数
+y = torch.tanh(x)
+d2l.plot(x.detach(), y.detach(), 'x', 'tanh(x)', figsize=(5, 2.5))
+d2l.plt.show()
+# 清除以前的梯度
+x.grad.data.zero_()
+y.backward(torch.ones_like(x), retain_graph=True)
+d2l.plot(x.detach(), x.grad, 'x', 'grad of tanh', figsize=(5, 2.5))
+d2l.plt.show()
+
+
